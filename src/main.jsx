628678a8a6b2378e08b26469635dab1663740658
ELOIJOHN <eloigroups@gmail.com>
2025-10-07 17:37:27 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import ArchiAtechWebsite from './archiatech-website.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ArchiAtechWebsite />
+    <App />
   </StrictMode>,
 )
