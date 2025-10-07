9e7e0db9a4d6647652d7566a34a1d8020279aeda
ELOIJOHN <eloigroups@gmail.com>
2025-10-07 22:39:04 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Config unifiée: domaine custom via CNAME => base '/'
 export default defineConfig({
   plugins: [react()],
-  base: '/archiatech-website/',
-  build: {
-    outDir: 'dist',
-  },
 })
